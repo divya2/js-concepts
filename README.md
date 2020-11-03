5a468eed406b9915c8b4ffd1f456a083ade40316
divya2 <divu.0209@gmail.com>
2020-11-03 15:49:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-tc52we
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tc52we)
---
+++ index.js
@@ -0,0 +1,27 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+//Variables - used to store this information
+//Hoisting
+hoistingVar = "Hello";
+var hoistingVar;
+
+//Temporal dead zone
+
+/* letKeyword ="Hi";
+let letKeyword; //Cannot access 'letKeyword' before initialization
+*/
+
+// Tasks
+/*Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).*/
+
+let name = "John";
+let admin = name;
+//alert(admin);
