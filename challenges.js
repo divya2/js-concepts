26a2890e0f2dbe50b5065b7cc8ea414c2a1c1e73
divya2 <divu.0209@gmail.com>
2020-11-04 22:12:38 +0530

    1 file modified

--- challenges.js
+++ challenges.js
@@ -6,3 +6,20 @@ nextPrime: for (let i = 2; i <= n; i++) {
   }
   console.log(i);
 }
+
+//Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+//uncomment while running
+/*  
+//solution-1
+let i = prompt("enter a number greater than 100", ""); 
+if (i < 100) {
+  let i = prompt("enter a number greater than 100", "");
+} else {
+  console.log("\n");
+}
+//solution-2
+let num;
+do {
+  num = prompt("Enter a number greater than 100?", 0); 
+} while (num <= 100 && num);
+*/
